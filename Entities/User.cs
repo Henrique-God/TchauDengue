3e9d010cba1e9b6c757601cbe9a282be1ff543a3
Henrique-God <henriquergodoy07@gmail.com>
2024-09-25 13:47:21 -0300

    First commit, small adjustments

---
+++ Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TchauDengue.Entities
+{
+    public class User
+    {
+        //Tenho que ver certinho algumas coisas ainda
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        
+    }
+}
