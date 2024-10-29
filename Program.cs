eda87a28ecf28a07defec3a7e7609b66582cd843
Henrique-God <henriquergodoy07@gmail.com>
2024-10-28 22:31:09 -0300

    DataContext Added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using TchauDengue.Providers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string conection = Environment.GetEnvironmentVariable("DATABASE_CONECTION_STRING");
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(conection);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Providers/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TchauDengue.Entities;
+
+namespace TchauDengue.Providers
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users {  get; set; }
+    }
+}
