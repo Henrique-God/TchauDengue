2a1bcd59be52f48d9b1ce8826aba2752d9a77ae8
Henrique-God <henriquergodoy07@gmail.com>
2024-11-12 13:45:46 -0300

    required added to user entity

--- Entities/User.cs
+++ Entities/User.cs
@@ -3,13 +3,13 @@
     public class User
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string ZipCode { get; set; }
-        public int SocialNumber { get; set; }
+        public required string UserName { get; set; }
+        public required byte[] PasswordHash { get; set; }
+        public required byte[] PasswordSalt { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string ZipCode { get; set; }
+        public required int SocialNumber { get; set; }
         
     }
 }
