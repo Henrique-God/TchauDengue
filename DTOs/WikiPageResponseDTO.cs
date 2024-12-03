57007d6d4f0e52a689ba922f1483c890f2e9f4a1
gabrielzitoo <gabriel08.set.2000@gmail.com>
2024-12-03 01:45:04 -0300

    validated added to DTO

--- DTOs/WikiPageResponseDTO.cs
+++ DTOs/WikiPageResponseDTO.cs
@@ -6,6 +6,7 @@ public class WikiPageResponseDTO
     public string PageText { get; set; }
     public string PageTitle { get; set; }
     public string OwnerName { get; set; }
+    public bool Validated { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public List<PageHistoryResponseDTO> History { get; set; }
@@ -16,6 +17,7 @@ public class WikiPageResponseDTO
         this.PageText = wikiPage.PageText;
         this.PageTitle = wikiPage.PageTitle;
         this.OwnerName = userName ?? "";
+        this.Validated = wikiPage.Validated;
         this.UpdatedAt = wikiPage.UpdatedAt;
         this.History = wikiPage.History
             .Select(ph => new PageHistoryResponseDTO(ph))
@@ -28,6 +30,7 @@ public class WikiPageResponseDTO
         this.PageText = wikiPage.PageText;
         this.PageTitle = wikiPage.PageTitle;
         this.OwnerName = "";
+        this.Validated = wikiPage.Validated;
         this.UpdatedAt = wikiPage.UpdatedAt;
         this.History = wikiPage.History
             .Select(ph => new PageHistoryResponseDTO(ph))
